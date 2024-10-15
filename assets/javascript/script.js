d52648eec5e3ba2d85700741435c7250383234ce
jevpr <jevprice@googlemail.com>
2024-10-15 15:32:08 +0100

    update2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,6 @@ img.addEventListener("click", revealMenu);
 
 function revealMenu() {
   const x = document.getElementById("dropDown");
-  const menuLogo = document.getElementById("menuImg");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
