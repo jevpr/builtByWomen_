7b02a3337a9cb1113b40839daf94b2a2601c7a6e
jevpr <jevprice@googlemail.com>
2024-10-16 09:38:26 +0100

    Swirling logo

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,13 @@ function revealMenu() {
   const x = document.getElementById("dropDown");
   if (x.style.display === "none") {
     x.style.display = "block";
+    img.style.transform = "rotate(180deg)";
+    img.style.transition = "transform 2s";
   } else {
-    x.style.display = "none";
+    img.style.transform = "rotate(-180deg)";
+    img.style.transition = "transform 2s";
+    setTimeout(() => {
+      x.style.display = "none";
+    }, 1000);
   }
 }
