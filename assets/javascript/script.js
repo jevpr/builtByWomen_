19aab39d98fa297f70589cfefa779f20cd08f1d5
jevpr <jevprice@googlemail.com>
2024-10-16 10:03:59 +0100

    menuupdates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,15 +3,17 @@ img.addEventListener("click", revealMenu);
 
 function revealMenu() {
   const x = document.getElementById("dropDown");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    img.style.transform = "rotate(180deg)";
-    img.style.transition = "transform 2s";
-  } else {
+  if (x.style.display === "block") {
     img.style.transform = "rotate(-180deg)";
-    img.style.transition = "transform 2s";
+    img.style.transition = "transform 3s";
     setTimeout(() => {
       x.style.display = "none";
+    }, 200);
+  } else {
+    img.style.transform = "rotate(180deg)";
+    img.style.transition = "transform 2s";
+    setTimeout(() => {
+      x.style.display = "block";
     }, 1000);
   }
 }
